1b2f91d96e64b394de95bbf472a514c8d2a16794
Felipe Borba <108702072+oFelipeBorba@users.noreply.github.com>
2022-11-27 22:05:51 -0300

    add repository + integra com PostgreSQL

--- mudi/mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,28 +1,25 @@
 package br.com.alura.mvc.mudi.controller;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
-    //Outro modo de mandar um action para uma view, sem ser com request do servlet, e usando a classe model do spring, basta colocar na criacao do  metodo o Model model
-    // e depois usar ele para tratar um addAttribute por exemplo
-    public String home(Model model){
-        Pedido pedido = new Pedido();
-        pedido.setNomeProduto("Readmi Note 8");
-        pedido.setUrlImagem("https://http2.mlstatic.com/D_NQ_NP_620994-MLA40022201594_122019-O.webp");
-        pedido.setUrlProduto("https://www.mercadolivre.com.br/xiaomi-redmi-note-8-pro-dual-sim-128-gb-forest-green-8-gb-ram/p/MLB15239262?pdp_filters=category:MLB1055#searchVariation=MLB15239262&position=1&search_layout=stack&type=product&tracking_id=26373347-3873-40a9-bafc-6585b6f915ad");
-        pedido.setDescricao("aleatorio");
 
-        //crio uma Array do tipo list, que recebe objetos do tipo Pedido, eu passo para dentro array como list, se eu colocar vaiors "pedido" dentro do
-        //asList ele ira rederizar todos no home.html, pq la eu passei um th:each de um atributo que chamei de pedidos, em model.addAttribute
-        List<Pedido> listaPedidos = Arrays.asList(pedido);
+    @Autowired
+    private PedidoRepository repository;
+
+    @GetMapping("/home")
+    public String home(Model model){
+
+        List<Pedido> listaPedidos = repository.findAll();
         model.addAttribute("pedidos", listaPedidos);
 
         return "home";
---
+++ mudi/mudi/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -0,0 +1,26 @@
+package br.com.alura.mvc.mudi.repository;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+    //Metodo JPA Puro, com class e nao interface
+    //Crio um persistenceContext no EntityManager, para fazer as requisicoes (Ex, JPA puro)
+//    @PersistenceContext
+//    private EntityManager entityManager;
+    //Esse metodo e de JPA Puro, para fazer o mesmo com spring data Jpa, basta usar um findAll() que ele ira entender
+//    public List<Pedido> recuperaTodosOsPedidos(){
+//        //Query para fazer uma consulta (Ex, JPA puro)
+//        Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+//        return query.getResultList();
+//    }
+
+}
